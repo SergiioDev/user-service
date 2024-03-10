854dd0c5090cd28c1cdf94f39aec50d3e0f0923d
SergiioDev <sergi13456@hotmail.es>
2024-03-10 23:20:23 +0100

    create JwService and update pom.xml

---
+++ src/main/java/com/ecommerce/userservice/service/JwtService.java
@@ -0,0 +1,85 @@
+package com.ecommerce.userservice.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.ecommerce.userservice.models.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${spring.custom.secret}")
+    private String secret;
+
+
+    private String createToken(User user) {
+        long expirationTimeMillis = 3600000L;
+        Date issuedAt = new Date();
+        Date expirationTime = new Date(issuedAt.getTime() + expirationTimeMillis);
+
+        return JWT.create()
+                .withClaim("email", user.getEmail())
+                .withClaim("username", user.getUsername())
+                .withIssuer("e-commerce")
+                .withIssuedAt(issuedAt)
+                .withExpiresAt(expirationTime)
+                .withJWTId(generateSecureRandomJwtId())
+                .sign(Algorithm.HMAC512(secret));
+    }
+
+
+    private String generateSecureRandomJwtId() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] randomBytes = new byte[32];
+        secureRandom.nextBytes(randomBytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractEmail(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
