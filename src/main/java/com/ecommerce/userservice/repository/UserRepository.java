7e3fc637779fcab2a2a5e4374adcb5b6e6d51cb5
SergiioDev <sergi13456@hotmail.es>
2024-03-25 20:34:01 +0100

    create UserDetailService and update UserRepository

--- src/main/java/com/ecommerce/userservice/repository/UserRepository.java
+++ src/main/java/com/ecommerce/userservice/repository/UserRepository.java
@@ -3,10 +3,15 @@ package com.ecommerce.userservice.repository;
 import com.ecommerce.userservice.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM users u where u.email = :email")
+    @Query("SELECT u FROM User u where u.email = :email")
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u where u.username = :username")
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/ecommerce/userservice/security/UserDetailService.java
@@ -0,0 +1,28 @@
+package com.ecommerce.userservice.security;
+
+import com.ecommerce.userservice.exceptions.UserNotFoundException;
+import com.ecommerce.userservice.models.entity.User;
+import com.ecommerce.userservice.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return UserPrinciple.build(user);
+    }
+}
